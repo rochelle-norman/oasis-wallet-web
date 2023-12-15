8cb77aadcf6e2a7c5e9f1d86c998eb4033b7a3e2
lukaw3d <lukajeran@oasislabs.com>
2023-12-15 17:19:19 +0100

    use cimode language

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -47,6 +47,7 @@ export const i18n = i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources: translationsJson,
+    lng: 'cimode',
     fallbackLng: 'en',
     debug: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test',
 
