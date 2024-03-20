ada371063aadd658c1ea2002aab573c19efa06bd
lukaw3d <lukajeran@oasislabs.com>
2024-03-20 03:24:37 +0100

    auto-open an account

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,38 @@ import './locales/i18n'
 import './styles/main.css'
 import { routes } from './routes'
 
-const store = configureAppStore()
+const store = configureAppStore({
+  theme: {
+    selected: 'light',
+  },
+  wallet: {
+    wallets: {
+      oasis1qz78ap0456g2rk7j6rmtvasc9v2kjhz2s58qgj90: {
+        address: 'oasis1qz78ap0456g2rk7j6rmtvasc9v2kjhz2s58qgj90',
+        publicKey: 'ad4bca4ab8759f2a6f71db08a913599df6f63a2bae5a7c48f637be325861a51c',
+        privateKey: '<<redacted>>',
+        type: 'private_key' as any,
+        balance: {
+          address: 'oasis1qz78ap0456g2rk7j6rmtvasc9v2kjhz2s58qgj90',
+          allowances: [],
+          available: '24454060789',
+          delegations: '100945338678',
+          debonding: '0',
+          total: '125399399467',
+        },
+      },
+    },
+    selectedWallet: 'oasis1qz78ap0456g2rk7j6rmtvasc9v2kjhz2s58qgj90',
+  },
+  persist: {
+    hasPersistedProfiles: false,
+    hasV0StorageToMigrate: false,
+    isPersistenceUnsupported: false,
+    loading: false,
+    stringifiedEncryptionKey: 'skipped',
+    enteredWrongPassword: false,
+  },
+})
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container!)
 const router = createBrowserRouter(routes)
