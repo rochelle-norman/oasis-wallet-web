b496774a20bb6397d70f8ce80041f08b98d3880c
lukaw3d <lukajeran@oasislabs.com>
2024-04-24 04:18:35 +0200

    remove banner

--- src/app/components/BuildBanner/index.tsx
+++ src/app/components/BuildBanner/index.tsx
@@ -14,6 +14,7 @@ const StickyBanner = styled(Box)`
 
 export const BuildBanner = () => {
   const { t } = useTranslation()
+  return null
 
   if (window.location.origin === deploys.extension) {
     return null
